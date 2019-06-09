823d149ef9170b2ecd52f740ab8d0b6f95dc1e27
Mateusz Nawrocki <nawrockimateusz96@gmail.com>
2019-06-09 20:44:13 +0200

    fourier transform

--- Starter.py
+++ Starter.py
@@ -1,10 +1,14 @@
+
 import speech_recognition as sr
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.io.wavfile
+from scipy.fftpack import fft
+import sys
 
 from os import path
 AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "ENG_F.wav")
-AUDIO_FILE_fr = path.join(path.dirname(path.realpath(__file__)), "f_m3.wav")
+AUDIO_FILE_fr = path.join(path.dirname(path.realpath(__file__)), "french.aiff")
 
 
 def histogram(data, n_bins, cumulative=False, x_label="", y_label="", title=""):
@@ -13,6 +17,18 @@ def histogram(data, n_bins, cumulative=False, x_label="", y_label="", title=""):
     amplitude = np.fromstring(data, np.int16)
     s.plot(amplitude)
     fig.savefig('plot.png')
+
+
+def fourier_transform():
+    rate, data = scipy.io.wavfile.read('microphone-results.wav')
+    print(rate)
+    a = data  # this is a two channel soundtrack, I get the first track
+    b = [(ele / 2 ** 8.) * 2 - 1 for ele in data]  # this is 8-bit track, b is now normalized on [-1,1)
+    fft_out = fft(b)
+    d = len(fft_out) // 2
+    plt.plot(abs(fft_out[:(d - 1)]), 'r')
+    # plt.plot(rate, np.abs(fft_out))
+    plt.show()
 
 
 r = sr.Recognizer()
@@ -36,7 +52,7 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said in eng" + r.recognize_google(audio_en))
+    print("Google Speech Recognition thinks you said in eng " + r.recognize_google(audio_en))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
@@ -52,9 +68,13 @@ except sr.RequestError as e:
 # write audio to a WAV file
 with open("microphone-results.wav", "wb") as f:
     f.write(audio.get_wav_data())
-
+    fourier_transform()
 try:
     text = r.recognize_google(audio)
     print("You said: {}".format(text))
 except sr.UnknownValueError:
-    print("Cannot recognize what you said")
+    try:
+        text = r.recognize_google(audio, language='fr-FR')
+        print("You said: {}".format(text))
+    except sr.UnknownValueError:
+        print("Cannot recognize what you said")
